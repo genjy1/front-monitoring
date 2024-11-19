71555b067c26a8449118a9947d8ec4c646edc372
Daniil <dmayorov@vend-shop.com>
2024-11-19 14:25:55 +0300

    Сделал фильтр отображения колонок для страницы состояния автоматов

---
+++ src/stores/columns.js
@@ -0,0 +1,42 @@
+// columnsStore.js
+import { defineStore } from 'pinia'
+
+export const useColumnsStore = defineStore('columns', {
+  state: () => ({
+    number: [false, 'Номер'],
+    name: [false, 'Имя'],
+    address: [false, 'Адрес'],
+    status: [false, 'Статус'],
+    condition: [false, 'Состояние'],
+    errors: [false, 'Ошибки'],
+    waterLevel: [false, 'Уровень воды'],
+    temperature: [false, 'Температура'],
+    cashCounter: [false, 'Счётчик денег'],
+    cashBox: [false, 'Кэшбокс'],
+    stacker: [false, 'Стэкер'],
+    change: [false, 'Сдача'],
+    tubes: [false, 'Тубы'],
+    recycling: [false, 'Рециклинг'],
+    balance: [false, 'Баланс'],
+    goodsSold: [false, 'Продано товаров'],
+    revenue: [false, 'Выручка'],
+  }),
+  actions: {
+    toggleColumn(column) {
+      column[0] = !column[0] // Переключение состояния видимости столбца
+      this.saveColumns() // Сохраняем изменения сразу
+    },
+    loadColumns() {
+      const savedColumns = localStorage.getItem('columns')
+      if (savedColumns) {
+        const parsedColumns = JSON.parse(savedColumns)
+        Object.keys(parsedColumns).forEach((key) => {
+          this[key] = parsedColumns[key]
+        })
+      }
+    },
+    saveColumns() {
+      localStorage.setItem('columns', JSON.stringify(this.$state))
+    },
+  },
+})
--- src/views/machines/MachineState.vue
+++ src/views/machines/MachineState.vue
@@ -1,40 +1,123 @@
 <template>
   <div>
-    <!-- Добавляем единственный корневой элемент -->
     <HeaderComponent />
     <div class="container mt-24 mx-auto w-4/5">
       <ViewHeader :text="header" />
       <hr class="my-2" />
+      <div class="filter-wrapper grid grid-cols-8 gap-2">
+        <button
+          v-for="column in columns"
+          @click="toggleColumn(column)"
+          class="rounded border border-[#6B23A7] px-2 py-1"
+          :inner-h-t-m-l="column[0] === true ? 'Показать' : 'Скрыть'"
+        >
+          {{ column[1] }}
+        </button>
+      </div>
       <table class="hidden sm:table table-auto w-full border mt-4">
         <thead>
           <tr class="border-b bg-[#eee]">
             <th class="border-r py-2 font-normal">#</th>
-            <th class="border-r py-2 font-normal">Номер</th>
-            <th class="border-r py-2 font-normal">Статус</th>
-            <th class="border-r py-2 font-normal">Состояние</th>
-            <th class="border-r py-2 font-normal">Счётчик денег</th>
-            <th class="border-r py-2 font-normal">Сдача</th>
-            <th class="border-r py-2 font-normal">Баланс</th>
-            <th class="border-r py-2 font-normal">Продано товаров</th>
-            <th class="border-r py-2 font-normal">Выручка</th>
+            <transition name="fade" v-if="columns.number && columns.number[0] === true">
+              <th class="border-r py-2 font-normal">Номер</th>
+            </transition>
+            <transition name="fade" v-if="columns.name && columns.name[0] === true">
+              <th class="border-r py-2 font-normal">Имя</th>
+            </transition>
+            <transition name="fade" v-if="columns.address && columns.address[0] === true">
+              <th class="border-r py-2 font-normal">Адрес</th>
+            </transition>
+            <transition name="fade" v-if="columns.status && columns.status[0] === true">
+              <th class="border-r py-2 font-normal">Статус</th>
+            </transition>
+            <transition name="fade" v-if="columns.condition && columns.condition[0] === true">
+              <th class="border-r py-2 font-normal">Состояние</th>
+            </transition>
+            <transition name="fade" v-if="columns.errors && columns.errors[0] === true">
+              <th class="border-r py-2 font-normal">Ошибки</th>
+            </transition>
+            <transition name="fade" v-if="columns.waterLevel && columns.waterLevel[0] === true">
+              <th class="border-r py-2 font-normal">Уровень воды</th>
+            </transition>
+            <transition name="fade" v-if="columns.temperature && columns.temperature[0] === true">
+              <th class="border-r py-2 font-normal">Температура</th>
+            </transition>
+            <transition name="fade" v-if="columns.cashCounter && columns.cashCounter[0] === true">
+              <th class="border-r py-2 font-normal">Счётчик денег</th>
+            </transition>
+            <transition name="fade" v-if="columns.cashBox && columns.cashBox[0] === true">
+              <th class="border-r py-2 font-normal">Кэшбокс</th>
+            </transition>
+            <transition name="fade" v-if="columns.stacker && columns.stacker[0] === true">
+              <th class="border-r py-2 font-normal">Стэкер</th>
+            </transition>
+            <transition name="fade" v-if="columns.change && columns.change[0] === true">
+              <th class="border-r py-2 font-normal">Сдача</th>
+            </transition>
+            <transition name="fade" v-if="columns.tubes && columns.tubes[0] === true">
+              <th class="border-r py-2 font-normal">Тубы</th>
+            </transition>
+            <transition name="fade" v-if="columns.recycling && columns.recycling[0] === true">
+              <th class="border-r py-2 font-normal">Рециклинг</th>
+            </transition>
+            <transition name="fade" v-if="columns.balance && columns.balance[0] === true">
+              <th class="border-r py-2 font-normal">Баланс</th>
+            </transition>
+            <transition name="fade" v-if="columns.goodsSold && columns.goodsSold[0] === true">
+              <th class="border-r py-2 font-normal">Продано товаров</th>
+            </transition>
+            <transition name="fade" v-if="columns.revenue && columns.revenue[0] === true">
+              <th class="border-r py-2 font-normal">Выручка</th>
+            </transition>
           </tr>
         </thead>
         <tbody>
           <tr v-for="machine in machines.data" :key="machine.id" class="border-b">
             <td class="py-2 text-center border-r">{{ machine.id }}</td>
-            <td class="py-2 border-r text-center">{{ machine.number }}</td>
-            <td
-              class="py-2 border-r text-center"
-              :class="machine.status === 'Online' ? 'text-green-800' : 'text-red-800'"
-            >
-              {{ machine.status }}
-            </td>
-            <td class="py-2 border-r text-center">{{ machine.condition }}</td>
-            <td class="py-2 border-r text-center">{{ machine.cash_counter }}</td>
-            <td class="py-2 border-r text-center">Cash</td>
-            <td class="py-2 border-r text-center">{{ machine.balance }}</td>
-            <td class="py-2 border-r text-center">{{ machine.goods_sold }}</td>
-            <td class="py-2 text-center">{{ machine.goods_total }}</td>
+            <transition name="fade" v-if="columns.number && columns.number[0] === true">
+              <td class="py-2 border-r text-center">{{ machine.number }}</td>
+            </transition>
+            <transition name="fade" v-if="columns.name && columns.name[0] === true">
+              <td class="py-2 border-r text-center">{{ machine.name }}</td>
+            </transition>
+            <transition name="fade" v-if="columns.address && columns.address[0] === true">
+              <td class="py-2 border-r text-center">{{ machine.address }}</td>
+            </transition>
+            <transition name="fade" v-if="columns.status && columns.status[0] === true">
+              <td
+                class="py-2 border-r text-center"
+                :class="machine.status === 'Online' ? 'text-green-800' : 'text-red-800'"
+              >
+                {{ machine.status }}
+              </td>
+            </transition>
+            <transition name="fade" v-if="columns.condition && columns.condition[0] === true">
+              <td class="py-2 border-r text-center">{{ machine.condition }}</td>
+            </transition>
+            <transition name="fade" v-if="columns.errors && columns.errors[0] === true">
+              <td class="py-2 border-r text-center">{{ machine.errors }}</td>
+            </transition>
+            <transition name="fade" v-if="columns.cashCounter && columns.cashCounter[0] === true">
+              <td class="py-2 border-r text-center">{{ machine.cash_counter }}</td>
+            </transition>
+            <transition name="fade" v-if="columns.change && columns.change[0] === true">
+              <td class="py-2 border-r text-center">{{ machine.change }}</td>
+            </transition>
+            <transition name="fade" v-if="columns.tubes && columns.tubes[0] === true">
+              <td class="py-2 border-r text-center">{{ machine.tubes }}</td>
+            </transition>
+            <transition name="fade" v-if="columns.recycling && columns.recycling[0] === true">
+              <td class="py-2 border-r text-center">{{ machine.recycling }}</td>
+            </transition>
+            <transition name="fade" v-if="columns.balance && columns.balance[0] === true">
+              <td class="py-2 border-r text-center">{{ machine.balance }}</td>
+            </transition>
+            <transition name="fade" v-if="columns.goodsSold && columns.goodsSold[0] === true">
+              <td class="py-2 border-r text-center">{{ machine.goods_sold }}</td>
+            </transition>
+            <transition name="fade" v-if="columns.revenue && columns.revenue[0] === true">
+              <td class="py-2 text-center">{{ machine.revenue }}</td>
+            </transition>
           </tr>
         </tbody>
       </table>
@@ -48,24 +131,53 @@ import HeaderComponent from '@/components/HeaderComponent.vue'
 import ViewHeader from '@/components/ViewHeader.vue'
 import Pagination from '@/components/Pagination.vue'
 import { useUserStore } from '@/stores/userStore'
+import { useColumnsStore } from '@/stores/columns'
 import axios from 'axios'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, onBeforeUnmount } from 'vue'
 
 const header = 'Состояние автоматов'
-const loading = ref('Загрузка данных...')
-const machines = ref({})
+const machines = ref([])
+
 const userStore = useUserStore()
+const columnsStore = useColumnsStore()
+const columns = columnsStore.$state // Используем все состояние хранилища
+
+const toggleColumn = (column) => {
+  columnsStore.toggleColumn(column) // Переключаем состояние и сохраняем
+}
 
 const onPageChange = async (url) => {
   const response = await axios.get(url)
   machines.value = response.data
 }
 
+// Сохраняем столбцы перед размонтированием компонента
+onBeforeUnmount(() => {
+  columnsStore.saveColumns()
+})
+
 onMounted(async () => {
   if (!userStore.user) {
-    await userStore.fetchUser() // вызовите метод для загрузки данных
+    await userStore.fetchUser()
   }
+
+  columnsStore.loadColumns() // Загружаем сохранённое состояние столбцов
+
   const response = await axios.get(`http://127.0.0.1:8000/api/user/${userStore.user.id}/machines/`)
   machines.value = response.data
 })
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition:
+    opacity 0.5s ease,
+    transform 0.5s ease;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+</style>
